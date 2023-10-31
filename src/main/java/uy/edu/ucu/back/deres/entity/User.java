971dd4415e9e9b99ba77040255001c79c557d745
Facundo-Martiarena <facundo.martiarena@correo.ucu.edu.uy>
2023-10-31 20:27:52 -0300

    user constructor

--- src/main/java/uy/edu/ucu/back/deres/entity/User.java
+++ src/main/java/uy/edu/ucu/back/deres/entity/User.java
@@ -2,6 +2,7 @@ package uy.edu.ucu.back.deres.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,10 +11,16 @@ import java.util.UUID;
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
 public class User {
     @Id
     private UUID id;
     private String name;
     private String password;
     private Privilege privilege;
+
+
+    public User() {
+
+    }
 }
