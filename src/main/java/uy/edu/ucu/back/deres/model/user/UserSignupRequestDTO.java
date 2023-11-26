f04558e33afcb0f2061d767c7145dab032d50f9a
Facundo-Martiarena <facundo.martiarena@correo.ucu.edu.uy>
2023-11-25 21:22:39 -0300

    problemas con properties

--- src/main/java/uy/edu/ucu/back/deres/model/user/UserSignupRequestDTO.java
+++ src/main/java/uy/edu/ucu/back/deres/model/user/UserSignupRequestDTO.java
@@ -1,12 +1,9 @@
 package uy.edu.ucu.back.deres.model.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.UUID;
 
 @Setter
 @Getter
